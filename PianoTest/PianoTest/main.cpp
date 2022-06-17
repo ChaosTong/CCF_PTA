e58e9db80384b13e19f9a84d4b088f72e07ee1bc
ChaosTong <easyulife@gmail.com>
2022-06-17 20:15:56 +0800

    update

--- PianoTest/PianoTest/main.cpp
+++ PianoTest/PianoTest/main.cpp
@@ -5,7 +5,6 @@ int main() {
 #else
     freopen("f.in", "r", stdin);
 #endif
-<<<<<<< HEAD
     int m, n;
     int s[10][5] = {0};
     scanf("%d %d", &n, &m);
@@ -38,20 +37,6 @@ int main() {
         if (flag == 0) {
             printf("IGNORED\n");
         }
-=======
-    int n;
-    int mod = 1000000007;
-    long long s[6][1005] = {0};
-    scanf("%d", &n);
-    for (int len = 1; len <= n; ++len) {
-        s[0][len] = 1;
-        s[1][len] = (s[1][len-1]*2 + s[0][len-1])%mod;
-        s[2][len] = (s[2][len-1] + s[0][len-1])%mod;
-        s[3][len] = (s[3][len-1]*2 + s[1][len-1])%mod;
-        s[4][len] = (s[4][len-1]*2 + s[2][len-1] + s[1][len-1])%mod;
-        s[5][len] = (s[5][len-1]*2 + s[3][len-1] + s[4][len-1])%mod;
->>>>>>> c5b12446533b4a6097d48fd2eeb0ed844de26d28
     }
-    printf("%lld", s[5][n]);
     return 0;
 }
